123d65a63378c1171086d49748231917f3607fd3
SergejsBogdanovs <bogdanov.sergej@gmail.com>
2019-04-01 14:50:38 +0300

    Added firebase clound messaging

---
+++ presentation/src/main/java/lv/st/sbogdano/stobjekti/fcm/StObjectsMessagingService.kt
@@ -0,0 +1,58 @@
+package lv.st.sbogdano.stobjekti.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import lv.st.sbogdano.stobjekti.R
+import lv.st.sbogdano.stobjekti.main.MainActivity
+
+class StObjectsMessagingService: FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        super.onMessageReceived(remoteMessage)
+        createNotificationChannel()
+        remoteMessage?.let {
+            val title = it.notification?.title?: "Backup title"
+            val message = it.notification?.body?: "Backup message"
+            sendNotification(title, message)
+        }
+    }
+
+    private fun sendNotification(title: String, content: String) {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val notificationBuilder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.sadales)
+            .setContentTitle(title)
+            .setContentText(content)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(12345, notificationBuilder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, "channel name", importance)
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    companion object {
+        const val CHANNEL_ID = "firebase_playground_channel_id"
+    }
+}
